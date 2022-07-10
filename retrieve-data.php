cd051133184dd1b095af26b1a42c3d68f2124bb4
PCGameJK <j.kasperski@codeur.online>
2022-07-10 12:24:17 +0200

    update back

--- retrieve-data.php
+++ retrieve-data.php
@@ -5,18 +5,25 @@ header('Access-Control-Allow-Origin: *');
 require_once 'models/Database.php';
 // TODO : Créer une instance de la classe PDO (connexion à la base)
 $pdo = new Database;
-// TODO : Prépare et exécute la requête de lecture de la table (try/catch)
-try {
-    //code...
-    $req = "SELECT * FROM foundlost ";
-    $stmt = $pdo->getPDO()->prepare($req);
-    $resultat = $stmt->execute();
-    var_dump(json_encode($stmt->fetchAll(PDO::FETCH_ASSOC)));
-    $stmt->closeCursor();
-    if($resultat > 0){ 
-       $pdo->getPDO();
-     }
-} catch (\Throwable $th) {
-    //throw $th;
+extract($_GET);
+if (isset($object)&&($objet==true)) {
+
+    try {
+        //code...
+        $req = "SELECT * FROM foundlost ";
+        $stmt = $pdo->getPDO()->prepare($req);
+        $resultat = $stmt->execute();
+        var_dump(json_encode($stmt->fetchAll(PDO::FETCH_ASSOC)));
+        $stmt->closeCursor();
+        if($resultat > 0){ 
+           $pdo->getPDO();
+         }
+    } catch (\Throwable $th) {
+        //throw $th;
+    }    # code..
+}else {
+    var_dump("parametre non autorisé");
 }
+// TODO : Prépare et exécute la requête de lecture de la table (try/catch)
+
 ?>
