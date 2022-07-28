12b73a727c4f34747635526334abd1d2addf8d5a
codeuronline <j.kasperski@codeur.online>
2022-07-28 10:23:28 +0200

    update see

--- see.php
+++ see.php
@@ -9,7 +9,7 @@
     <title>Document</title>
 </head>
 
-SSUCCES ===
+SUCCES ===
 <pre class='xdebug-var-dump' dir='ltr'>
 <small>C:\laragon\www\ionicserver\manage-data.php:21:</small><small>string</small> <font color='#cc0000'>'Instance PDO crée'</font> <i>(length=18)</i>
 </pre>
@@ -17,42 +17,83 @@ SSUCCES ===
 <small>C:\laragon\www\ionicserver\manage-data.php:31:</small><small>string</small> <font color='#cc0000'>'key'</font> <i>(length=3)</i>
 </pre>
 <pre class='xdebug-var-dump' dir='ltr'>
-<small>C:\laragon\www\ionicserver\manage-data.php:31:</small><small>string</small> <font color='#cc0000'>'update'</font> <i>(length=6)</i>
+<small>C:\laragon\www\ionicserver\manage-data.php:31:</small><small>string</small> <font color='#cc0000'>'create'</font> <i>(length=6)</i>
 </pre>
 <pre class='xdebug-var-dump' dir='ltr'>
 <small>C:\laragon\www\ionicserver\manage-data.php:38:</small>
-<b>array</b> <i>(size=10)</i>
-  'id_object' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'2'</font> <i>(length=1)</i>
-  'status' <font color='#888a85'>=&gt;</font> <small>int</small> <font color='#4e9a06'>1</font>
-  'description' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'gdgdgd'</font> <i>(length=6)</i>
-  'location' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'gfdgdfgd'</font> <i>(length=8)</i>
-  'date' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'2022-07-28'</font> <i>(length=10)</i>
-  'firstname' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'gdgdgdf'</font> <i>(length=7)</i>
-  'lastname' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'gdgdgf'</font> <i>(length=6)</i>
-  'email' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'gfdgd@ghrgtr.fr'</font> <i>(length=15)</i>
-  'checkedpicture' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'0'</font> <i>(length=1)</i>
-  'filename' <font color='#888a85'>=&gt;</font> <font color='#3465a4'>null</font>
+<b>array</b> <i>(size=8)</i>
+  'description' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'hgffh'</font> <i>(length=5)</i>
+  'location' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'hfhfh'</font> <i>(length=5)</i>
+  'date' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'2022-07-20'</font> <i>(length=10)</i>
+  'firstname' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'hfghfhf'</font> <i>(length=7)</i>
+  'lastname' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'hfhff'</font> <i>(length=5)</i>
+  'email' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'hhff@free.fr'</font> <i>(length=12)</i>
+  'checkedpicture' <font color='#888a85'>=&gt;</font> <small>boolean</small> <font color='#75507b'>false</font>
+  'filename' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>''</font> <i>(length=0)</i>
+</pre><br />
+<font size='1'>
+    <table class='xdebug-error xe-notice' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
+        <tr>
+            <th align='left' bgcolor='#f57900' colspan="5"><span
+                    style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice:
+                Undefined index: id_object in C:\laragon\www\ionicserver\manage-data.php on line <i>39</i></th>
+        </tr>
+        <tr>
+            <th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th>
+        </tr>
+        <tr>
+            <th align='center' bgcolor='#eeeeec'>#</th>
+            <th align='left' bgcolor='#eeeeec'>Time</th>
+            <th align='left' bgcolor='#eeeeec'>Memory</th>
+            <th align='left' bgcolor='#eeeeec'>Function</th>
+            <th align='left' bgcolor='#eeeeec'>Location</th>
+        </tr>
+        <tr>
+            <td bgcolor='#eeeeec' align='center'>1</td>
+            <td bgcolor='#eeeeec' align='center'>0.0008</td>
+            <td bgcolor='#eeeeec' align='right'>447192</td>
+            <td bgcolor='#eeeeec'>{main}( )</td>
+            <td title='C:\laragon\www\ionicserver\manage-data.php' bgcolor='#eeeeec'>...\manage-data.php<b>:</b>0</td>
+        </tr>
+    </table>
+</font>
+<br />
+<font size='1'>
+    <table class='xdebug-error xe-notice' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
+        <tr>
+            <th align='left' bgcolor='#f57900' colspan="5"><span
+                    style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice:
+                Undefined index: status in C:\laragon\www\ionicserver\manage-data.php on line <i>41</i></th>
+        </tr>
+        <tr>
+            <th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th>
+        </tr>
+        <tr>
+            <th align='center' bgcolor='#eeeeec'>#</th>
+            <th align='left' bgcolor='#eeeeec'>Time</th>
+            <th align='left' bgcolor='#eeeeec'>Memory</th>
+            <th align='left' bgcolor='#eeeeec'>Function</th>
+            <th align='left' bgcolor='#eeeeec'>Location</th>
+        </tr>
+        <tr>
+            <td bgcolor='#eeeeec' align='center'>1</td>
+            <td bgcolor='#eeeeec' align='center'>0.0008</td>
+            <td bgcolor='#eeeeec' align='right'>447192</td>
+            <td bgcolor='#eeeeec'>{main}( )</td>
+            <td title='C:\laragon\www\ionicserver\manage-data.php' bgcolor='#eeeeec'>...\manage-data.php<b>:</b>0</td>
+        </tr>
+    </table>
+</font>
+<pre class='xdebug-var-dump' dir='ltr'>
+<small>C:\laragon\www\ionicserver\manage-data.php:56:</small><small>string</small> <font color='#cc0000'>'CREATE DETECTE'</font> <i>(length=14)</i>
 </pre>
 <pre class='xdebug-var-dump' dir='ltr'>
-<small>C:\laragon\www\ionicserver\manage-data.php:115:</small><small>string</small> <font color='#cc0000'>'UPDATE DETECTE'</font> <i>(length=14)</i>
+<small>C:\laragon\www\ionicserver\manage-data.php:59:</small><small>boolean</small> <font color='#75507b'>false</font>
 </pre>
 <pre class='xdebug-var-dump' dir='ltr'>
-<small>C:\laragon\www\ionicserver\manage-data.php:121:</small><small>string</small> <font color='#cc0000'>'status'</font> <i>(length=6)</i>
-</pre>
-<pre class='xdebug-var-dump' dir='ltr'>
-<small>C:\laragon\www\ionicserver\manage-data.php:121:</small><small>boolean</small> <font color='#75507b'>true</font>
-</pre>
-<pre class='xdebug-var-dump' dir='ltr'>
-<small>C:\laragon\www\ionicserver\manage-data.php:123:</small><small>string</small> <font color='#cc0000'>'status'</font> <i>(length=6)</i>
-</pre>
-<pre class='xdebug-var-dump' dir='ltr'>
-<small>C:\laragon\www\ionicserver\manage-data.php:126:</small><small>string</small> <font color='#cc0000'>'date'</font> <i>(length=4)</i>
-</pre>
-<pre class='xdebug-var-dump' dir='ltr'>
-<small>C:\laragon\www\ionicserver\manage-data.php:132:</small><small>string</small> <font color='#cc0000'>'email'</font> <i>(length=5)</i>
-</pre>
-<pre class='xdebug-var-dump' dir='ltr'>
-<small>C:\laragon\www\ionicserver\manage-data.php:132:</small><small>string</small> <font color='#cc0000'>'gfdgd@ghrgtr.fr'</font> <i>(length=15)</i>
+<small>C:\laragon\www\ionicserver\manage-data.php:82:</small><small>string</small> <font color='#cc0000'>'INSERTION PRODUCT IN BD'</font> <i>(length=23)</i>
+</pre>agon\www\ionicserver\manage-data.php:132:</small><small>string</small>
+<font color='#cc0000'>'gfdgd@ghrgtr.fr'</font> <i>(length=15)</i>
 </pre>
 </body>
 
