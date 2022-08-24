f03c42d8aafd88e6f7c94d11f7c8402964cf83db
codeuronline <j.kasperski@codeur.online>
2022-08-24 13:47:04 +0200

    presentation annexe

--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
+    
     class Utilisateur{
-        private $user_name;
+        protected $user_name;
         protected $user_region;
         protected $prix_abo;
         protected $user_pass;
@@ -23,6 +24,7 @@
             .$valeur. '" (propriété inaccessible)<br>';
         }
     }
+   
     class Kid {
  
         /**
@@ -32,6 +34,7 @@
          * @access private
          */
         private $age;
+        private $valid_propriete= ["age","sexe","size"];
        
         /**
          * Methode magique __get()
@@ -44,8 +47,8 @@
          */
         public function __get($property) {
        
-          if('age' === $property) {
-            return $this->age;
+          if(in_array($property,$this->valid_propriete)) {
+            return $this->valid_propriete[$property];
           } else {
             throw new Exception('Propriété invalide !');
           }
@@ -64,11 +67,85 @@
         
         public function __set($property,$value) {
        
-          if('age' === $property && ctype_digit($value)) {
-            $this->age = (int) $value;  
+          if (in_array($property,$this->valid_propriete)) {
+            $this->valid_propriete[$property] = $value;  
           } else {
             throw new Exception('Propriété ou valeur invalide !');
           }
         }
       }
+
+
+class PropertyTest
+{
+    /**  Variable pour les données surchargées.  */
+    private $data = array();
+
+    /**  La surcharge n'est pas utilisée sur les propriétés déclarées.  */
+    public $declared = 1;
+
+    /**  La surcharge n'est lancée que lorsque l'on accède à cette propriété depuis l'extérieur de la classe.  */
+    private $hidden = 2;
+
+    public function __set($name, $value)
+    {
+        echo "Définition de '$name' à la valeur '$value'\n";
+        $this->data[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        echo "Récupération de '$name'\n";
+        if (array_key_exists($name, $this->data)) {
+            return $this->data[$name];
+        }
+
+        $trace = debug_backtrace();
+        trigger_error(
+            'Propriété non-définie via __get() : ' . $name .
+            ' dans ' . $trace[0]['file'] .
+            ' à la ligne ' . $trace[0]['line'],
+            E_USER_NOTICE);
+        return null;
+    }
+
+    public function __isset($name)
+    {
+        echo "Est-ce que '$name' est défini ?\n";
+        return isset($this->data[$name]);
+    }
+
+    public function __unset($name)
+    {
+        echo "Effacement de '$name'\n";
+        unset($this->data[$name]);
+    }
+
+    /**  Ce n'est pas une méthode magique, nécessaire ici que pour l'exemple.  */
+    public function getHidden()
+    {
+        return $this->hidden;
+    }
+}
+
+
+echo "<pre>\n";
+
+$obj = new PropertyTest;
+
+$obj->a = 1;
+echo $obj->a . "\n\n";
+
+var_dump(isset($obj->a));
+unset($obj->a);
+var_dump(isset($obj->a));
+echo "\n";
+
+echo $obj->declared . "\n\n";
+
+echo "Manipulons maintenant la propriété privée nommée 'hidden' :\n";
+echo "'hidden' est visible depuis la classe, donc __get() n'est pas utilisée...\n";
+echo $obj->getHidden() . "\n";
+echo "'hidden' n'est pas visible en dehors de la classe, donc __get() est utilisée...\n";
+echo $obj->hidden . "\n";
 ?>
--- visual.php
+++ visual.php
@@ -1,26 +1,50 @@
 <?php
 include('test.php');
 var_dump("exemple avec utlisateur");
+echo "<hr>";
 $propriete="age";
 $element = new Utilisateur;
-$element->__set($propriete,5);
+
+var_dump("assignation avec la methode magique");
+$element->__set($propriete,55);
+echo "<hr>";
+
+var_dump("assignation directe");
+$element->age="52";
+echo "<hr>";
+
+var_dump("assignation avec la methode magique d'une propriete private");
 $element->__set("user_name","bob");
+echo "<hr>";
+var_dump("recuperation d'une proporiete non définie par la methode magique");
 $element->__get($propriete);
+echo "<hr>";
+
+var_dump("recuperation d'une propriete defini par la methode magique");
 $element->__get("user_name");
+echo "<hr>";
+var_dump("affichage de l'objet");
 var_dump($element);
 echo "<hr>";
+
 $aKid= new Kid;
-var_dump("exemple avec enfant");
+var_dump("exemple avec kid");
+echo "<hr>";
+var_dump("affichage d'objet kid ");
 var_dump($aKid);
-$akid->age="12";
+echo "<hr>";
+$aKid->age="12";
+var_dump("acces à la variable directement");
 echo $aKid->age;
+var_dump($aKid);
 echo "<hr>";
-var_dump($akid2);
 $aKid2=new Kid;
-$aKid2->age=10;
+$aKid2->sexe="male";
+var_dump($aKid2);
 echo "<hr>";
-echo $aKid2->age;
+$aKid2->__set("sexe","female");
+$aKid2->__set("size",55);
 echo "<hr>";
-var_dump($akid2);
+var_dump($aKid2);
 echo "<hr>";
-echo $aKid2->age;
+echo $aKid2->__get("sexe")."//".$aKid2->__get("size");
