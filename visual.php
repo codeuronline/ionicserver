04f26a656fc2fb8557a77632bc04f76328f6639b
codeuronline <j.kasperski@codeur.online>
2022-08-25 16:30:15 +0200

    Revert "update"
    This reverts commit bb6149427e1cc54036e78f7186eb330cf1aca0b6.

--- visual.php
+++ visual.php
@@ -1,33 +1,32 @@
 <?php
-include('class.php');
-var_dump("exemple avec utilisateur");
+include('test.php');
+var_dump("exemple avec utlisateur");
 echo "<hr>";
-echo "<hr>";
-
 $propriete="age";
 $element = new Utilisateur;
 
-var_dump("assignation directe de la propriete age avec la valeur 50");
-$element->age=50;
-$element->addresse="ffdfdifhdihufdshfjdskjsdfhjfksd";
-// affichage de l'objet
-var_dump($element);
+var_dump("assignation avec la methode magique");
+$element->__set($propriete,55);
+echo "<hr>";
 
+var_dump("assignation directe");
+$element->age="52";
 echo "<hr>";
-var_dump("assignation directe de la propriété firstname avec la valeur bob");
-$element->firstname="bob";
-echo $element->firstname;
+
+var_dump("assignation avec la methode magique d'une propriete private");
+$element->__set("user_name","bob");
 echo "<hr>";
-var_dump($element)
-;var_dump("modification directe de la propriété age avec la valeur 15");
-$element->age=15;
-// affichage de l'objet
+var_dump("recuperation d'une proporiete non définie par la methode magique");
+$element->__get($propriete);
+echo "<hr>";
+
+var_dump("recuperation d'une propriete defini par la methode magique");
+$element->__get("user_name");
+echo "<hr>";
+var_dump("affichage de l'objet");
 var_dump($element);
-var_dump("recuperation d'une propriete");
-echo $element->lastname;
 echo "<hr>";
-echo "<hr>";
-// autre exemple objet kid
+
 $aKid= new Kid;
 var_dump("exemple avec kid");
 echo "<hr>";
@@ -35,7 +34,7 @@ var_dump("affichage d'objet kid ");
 var_dump($aKid);
 echo "<hr>";
 $aKid->age="12";
-var_dump("acces à la propriété age directement ");
+var_dump("acces à la variable directement");
 echo $aKid->age;
 var_dump($aKid);
 echo "<hr>";
@@ -45,32 +44,10 @@ var_dump($aKid2);
 echo "<hr>";
 $aKid2->__set("sexe","female");
 $aKid2->__set("size",55);
-$aKid2->age=55;
 echo "<hr>";
 var_dump($aKid2);
 echo "<hr>";
 var_dump("acces avec les methodes magiques");
 echo $aKid2->__get("sexe")."//".$aKid2->__get("size")."<br>";
-var_dump("acces directement a la prorpiete bien mais stocké dans un tableau private");
-echo $aKid2->sexe."//".$aKid2->size."//".$aKid2->age."<br><hr>";
-
-var_dump("autre exemple objet propertyTest");
-echo "<pre>\n";
-
-$obj = new PropertyTest;
-
-$obj->a = 1;
-echo $obj->a . "\n\n";
-
-var_dump(isset($obj->a));
-unset($obj->a);
-var_dump(isset($obj->a));
-echo "\n";
-
-echo $obj->declared . "\n\n";
-
-echo "Manipulons maintenant la propriété privée nommée 'hidden' :\n";
-echo "'hidden' est visible depuis la classe, donc __get() n'est pas utilisée...\n";
-echo $obj->getHidden() . "\n";
-echo "'hidden' n'est pas visible en dehors de la classe, donc __get() est utilisée...\n";
-echo $obj->hidden . "\n";
+var_dump("acces directement a la prorpiete bien qu'elle soit declare private et dans un tableau");
+echo $aKid2->sexe."//".$aKid2->size."//".$aKid2->parent;
