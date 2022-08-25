77bc6802ff131bad66b06455897a63cf0977cebe
PCGameJK <j.kasperski@codeur.online>
2022-08-25 21:46:03 +0200

    update->security

--- image.php
+++ image.php
@@ -1,8 +1,21 @@
 <?php 
+function check_image_mime($tmpname){
+	$finfo = finfo_open(FILEINFO_MIME_TYPE);
+	$mtype = finfo_file($finfo, $tmpname);
+	if(strpos($mtype, 'image/') === 0){
+		return true;
+	} else {
+		return false;
+	}
+	finfo_close($finfo);
+}
+
+
 error_log(print_r($_FILES),1);
 if(isset($_FILES['photo']['name'])){
    // file name
-   
+   // Valid extensions
+   $valid_ext = array("gif","jpg","png","jpeg");
    $filename = $_POST['id'];
    // Location
    // necessite de recupere son vrai nom
@@ -12,12 +25,12 @@ if(isset($_FILES['photo']['name'])){
    $file_extension = pathinfo($location, PATHINFO_EXTENSION);
    $file_extension = strtolower($file_extension);
 
-   // Valid extensions
-   $valid_ext = array("gif","jpg","png","jpeg");
-
    $response = 0;
-   if(in_array(mime_content_type($file_extension),$valid_ext)){
+   if(in_array($file_extension,$valid_ext)){
+    //  if(check_image_mime($_FILES['photo']['name'])==true){
+   //if(in_array(mime_content_type($file_extension),$valid_ext)){
       // Upload file
+      var_dump(mime_content_type($location));
       if(move_uploaded_file($_FILES['photo']['tmp_name'],$location)){
          $response = 1;
       } 
