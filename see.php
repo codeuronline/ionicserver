85fe5076e83148c631f14c13eb8a81e01fe6e786
codeuronline <j.kasperski@codeur.online>
2022-09-02 11:48:38 +0200

    update security

--- see.php
+++ see.php
@@ -10,68 +10,64 @@
 </head>
 
 <body>
-SUCCES === <pre class='xdebug-var-dump' dir='ltr'>
-<small>C:\laragon\www\ionicserver\manage-data.php:79:</small>
-<b>array</b> <i>(size=2)</i>
-  'email_user' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'bouba@free.fr'</font> <i>(length=13)</i>
-  'password' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'aA@1frtsa'</font> <i>(length=9)</i>
-</pre><br />
-<font size='1'><table class='xdebug-error xe-notice' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
-<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice: Undefined index: id_object in C:\laragon\www\ionicserver\manage-data.php on line <i>80</i></th></tr>
-<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
-<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
-<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0009</td><td bgcolor='#eeeeec' align='right'>474952</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='C:\laragon\www\ionicserver\manage-data.php' bgcolor='#eeeeec'>...\manage-data.php<b>:</b>0</td></tr>
-</table></font>
-<br />
-<font size='1'><table class='xdebug-error xe-notice' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
-<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice: Undefined index: description in C:\laragon\www\ionicserver\manage-data.php on line <i>81</i></th></tr>
-<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
-<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
-<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0009</td><td bgcolor='#eeeeec' align='right'>474952</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='C:\laragon\www\ionicserver\manage-data.php' bgcolor='#eeeeec'>...\manage-data.php<b>:</b>0</td></tr>
-</table></font>
-<br />
-<font size='1'><table class='xdebug-error xe-notice' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
-<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice: Undefined index: status in C:\laragon\www\ionicserver\manage-data.php on line <i>82</i></th></tr>
-<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
-<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
-<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0009</td><td bgcolor='#eeeeec' align='right'>474952</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='C:\laragon\www\ionicserver\manage-data.php' bgcolor='#eeeeec'>...\manage-data.php<b>:</b>0</td></tr>
-</table></font>
-<br />
-<font size='1'><table class='xdebug-error xe-notice' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
-<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice: Undefined index: date in C:\laragon\www\ionicserver\manage-data.php on line <i>83</i></th></tr>
-<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
-<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
-<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0009</td><td bgcolor='#eeeeec' align='right'>474952</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='C:\laragon\www\ionicserver\manage-data.php' bgcolor='#eeeeec'>...\manage-data.php<b>:</b>0</td></tr>
-</table></font>
-<br />
-<font size='1'><table class='xdebug-error xe-notice' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
-<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice: Undefined index: location in C:\laragon\www\ionicserver\manage-data.php on line <i>84</i></th></tr>
-<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
-<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
-<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0009</td><td bgcolor='#eeeeec' align='right'>474952</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='C:\laragon\www\ionicserver\manage-data.php' bgcolor='#eeeeec'>...\manage-data.php<b>:</b>0</td></tr>
-</table></font>
-<br />
-<font size='1'><table class='xdebug-error xe-notice' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
-<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice: Undefined index: firstname in C:\laragon\www\ionicserver\manage-data.php on line <i>85</i></th></tr>
-<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
-<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
-<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0009</td><td bgcolor='#eeeeec' align='right'>474952</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='C:\laragon\www\ionicserver\manage-data.php' bgcolor='#eeeeec'>...\manage-data.php<b>:</b>0</td></tr>
-</table></font>
-<br />
-<font size='1'><table class='xdebug-error xe-notice' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
-<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice: Undefined index: lastname in C:\laragon\www\ionicserver\manage-data.php on line <i>86</i></th></tr>
-<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
-<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
-<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0009</td><td bgcolor='#eeeeec' align='right'>474952</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='C:\laragon\www\ionicserver\manage-data.php' bgcolor='#eeeeec'>...\manage-data.php<b>:</b>0</td></tr>
-</table></font>
-<br />
-<font size='1'><table class='xdebug-error xe-notice' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
-<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice: Undefined index: email in C:\laragon\www\ionicserver\manage-data.php on line <i>87</i></th></tr>
-<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
-<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
-<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0009</td><td bgcolor='#eeeeec' align='right'>474952</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='C:\laragon\www\ionicserver\manage-data.php' bgcolor='#eeeeec'>...\manage-data.php<b>:</b>0</td></tr>
-</table></font>
-false
+    <br />
+    <font size='1'>
+        <table class='xdebug-error xe-notice' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
+            <tr>
+                <th align='left' bgcolor='#f57900' colspan="5"><span
+                        style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice:
+                    Undefined variable: var_dump in C:\laragon\www\ionicserver\manage-data.php on line <i>418</i></th>
+            </tr>
+            <tr>
+                <th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th>
+            </tr>
+            <tr>
+                <th align='center' bgcolor='#eeeeec'>#</th>
+                <th align='left' bgcolor='#eeeeec'>Time</th>
+                <th align='left' bgcolor='#eeeeec'>Memory</th>
+                <th align='left' bgcolor='#eeeeec'>Function</th>
+                <th align='left' bgcolor='#eeeeec'>Location</th>
+            </tr>
+            <tr>
+                <td bgcolor='#eeeeec' align='center'>1</td>
+                <td bgcolor='#eeeeec' align='center'>0.0008</td>
+                <td bgcolor='#eeeeec' align='right'>475032</td>
+                <td bgcolor='#eeeeec'>{main}( )</td>
+                <td title='C:\laragon\www\ionicserver\manage-data.php' bgcolor='#eeeeec'>...\manage-data.php<b>:</b>0
+                </td>
+            </tr>
+        </table>
+    </font>
+    SUCCES === <br />
+    <font size='1'>
+        <table class='xdebug-error xe-notice' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
+            <tr>
+                <th align='left' bgcolor='#f57900' colspan="5"><span
+                        style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice:
+                    Trying to access array offset on value of type bool in C:\laragon\www\ionicserver\manage-data.php on
+                    line <i>418</i></th>
+            </tr>
+            <tr>
+                <th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th>
+            </tr>
+            <tr>
+                <th align='center' bgcolor='#eeeeec'>#</th>
+                <th align='left' bgcolor='#eeeeec'>Time</th>
+                <th align='left' bgcolor='#eeeeec'>Memory</th>
+                <th align='left' bgcolor='#eeeeec'>Function</th>
+                <th align='left' bgcolor='#eeeeec'>Location</th>
+            </tr>
+            <tr>
+                <td bgcolor='#eeeeec' align='center'>1</td>
+                <td bgcolor='#eeeeec' align='center'>0.0009</td>
+                <td bgcolor='#eeeeec' align='right'>474968</td>
+                <td bgcolor='#eeeeec'>{main}( )</td>
+                <td title='C:\laragon\www\ionicserver\manage-data.php' bgcolor='#eeeeec'>...\manage-data.php<b>:</b>0
+                </td>
+            </tr>
+        </table>
+    </font>
+    false
 </body>
 
 </html>
