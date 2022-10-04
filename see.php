429b33b8d54caf7c6f6cba3487a6badb958bb8e8
PCGameJK <j.kasperski@codeur.online>
2022-10-04 06:41:07 +0200

    update ->image content

--- see.php
+++ see.php
@@ -15,7 +15,7 @@
             <tr>
                 <th align='left' bgcolor='#f57900' colspan="5"><span
                         style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice:
-                    Undefined index: user_id in C:\laragon\www\ionicserver\manage-data.php on line <i>42</i></th>
+                    Undefined index: user_id in C:\laragon\www\ionicserver\manage-data.php on line <i>61</i></th>
             </tr>
             <tr>
                 <th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th>
@@ -29,52 +29,18 @@
             </tr>
             <tr>
                 <td bgcolor='#eeeeec' align='center'>1</td>
-                <td bgcolor='#eeeeec' align='center'>0.0006</td>
-                <td bgcolor='#eeeeec' align='right'>471832</td>
+                <td bgcolor='#eeeeec' align='center'>0.0005</td>
+                <td bgcolor='#eeeeec' align='right'>471544</td>
                 <td bgcolor='#eeeeec'>{main}( )</td>
                 <td title='C:\laragon\www\ionicserver\manage-data.php' bgcolor='#eeeeec'>...\manage-data.php<b>:</b>0
                 </td>
             </tr>
         </table>
     </font>
-    <br />
-    <font size='1'>
-        <table class='xdebug-error xe-notice' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
-            <tr>
-                <th align='left' bgcolor='#f57900' colspan="5"><span
-                        style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice:
-                    Undefined index: id_object in C:\laragon\www\ionicserver\manage-data.php on line <i>43</i></th>
-            </tr>
-            <tr>
-                <th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th>
-            </tr>
-            <tr>
-                <th align='center' bgcolor='#eeeeec'>#</th>
-                <th align='left' bgcolor='#eeeeec'>Time</th>
-                <th align='left' bgcolor='#eeeeec'>Memory</th>
-                <th align='left' bgcolor='#eeeeec'>Function</th>
-                <th align='left' bgcolor='#eeeeec'>Location</th>
-            </tr>
-            <tr>
-                <td bgcolor='#eeeeec' align='center'>1</td>
-                <td bgcolor='#eeeeec' align='center'>0.0006</td>
-                <td bgcolor='#eeeeec' align='right'>471832</td>
-                <td bgcolor='#eeeeec'>{main}( )</td>
-                <td title='C:\laragon\www\ionicserver\manage-data.php' bgcolor='#eeeeec'>...\manage-data.php<b>:</b>0
-                </td>
-            </tr>
-        </table>
-    </font>
+    DELETE DETECTE
     <pre class='xdebug-var-dump' dir='ltr'>
-<small>C:\laragon\www\ionicserver\manage-data.php:68:</small><small>string</small> <font color='#cc0000'>'CREATE DETECTE'</font> <i>(length=14)</i>
-</pre>
-    <pre class='xdebug-var-dump' dir='ltr'>
-<small>C:\laragon\www\ionicserver\manage-data.php:106:</small><small>string</small> <font color='#cc0000'>'Problème Insertion sur user_id'</font> <i>(length=31)</i>
-</pre>
-    <pre class='xdebug-var-dump' dir='ltr'>
-<small>C:\laragon\www\ionicserver\manage-data.php:106:</small><small>int</small> <font color='#4e9a06'>0</font>
-</pre>
-    lostlist.page.ts:115 searchStatus true
+<small>C:\laragon\www\ionicserver\manage-data.php:269:</small><small>string</small> <font color='#cc0000'>'22'</font> <i>(length=2)</i>
+</pre>SUPPRESSION PRODUCT IN BD
 </body>
 
 </html>
